1ad5d4228e901633bdfa4644f4d26983e8d394f4
Tú Nguyễn <tusgrace1306@gmail.com>
2023-11-29 17:12:42 +0700

    update

--- components/telegram-bot-card.js
+++ components/telegram-bot-card.js
@@ -11,7 +11,7 @@ const TelegramBotCard = () => {
             src="/asset-12x-1@2x.png"
           />
           <div className="text-xl px-[16px] lg:px-0 leading-[120%] mb-[24px] font-semibold inline-block ">
-            Embark on your DeFi journey effortlessly - No confusing blockchain talk, just fast and budget - friendly trading on Telegram.
+            Explore decentralized finance without the complications. Our user - friendly Telegram bot takes care of all the technical details, enabling you to trade quickly and cost - effectively with just a few taps
           </div>
           <div className="leading-[120%] px-[16px] lg:px-0 mb-[48px] font-semibold inline-block ">
             Explore decentralized finance without the complications. Our user - friendly Telegram bot takes care of all the technical details, enabling you to trade quickly and cost - effectively with just a few taps.
