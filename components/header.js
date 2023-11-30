ddc6c7b2f508745b62fdc7c78964d6cee6428280
Tú Nguyễn <tusgrace1306@gmail.com>
2023-11-30 10:13:11 +0700

    update

--- components/header.js
+++ components/header.js
@@ -25,7 +25,8 @@ const Header = () => {
     <div className="relative w-full h-auto text-center text-13xl text-garbi-version-2-30-white">
       <Head>
         <meta property="og:title" content="ScroBot: Elevate Your Crypto Trading with AI-Powered Precision on Telegram!" />
-
+        <meta property="og:description" content="ScroBot is the ultimate Telegram based platform. Empower your journey with advanced AI tools, intelligent trading features, and a revolutionary tokenomics system. Join us where innovation meets accessibility, shaping a new era in crypto trading!" />
+        <meta property="og:image" content="/scrobot-for-social.png" />
       </Head>
       <div className="bg-garbi-version-2-60-black w-full h-auto overflow-hidden">
         <div className="container mx-auto py-[24px] overflow-hidden text-left text-base">
