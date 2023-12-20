118e83c21bf76af2a6566831d1a472c164fac23b
Tú Nguyễn <tusgrace1306@gmail.com>
2023-12-20 11:06:26 +0700

    update

--- components/stake.js
+++ components/stake.js
@@ -114,24 +114,24 @@ const Stake = () => {
         ) : (
             <p>No wallet connected</p>
         )}
-      <div className="overflow-hidden shrink-0 grid grid-cols-1 lg:grid-cols-5  pb-[68px] box-border gap-[90px]">
-        <div className='col-span-2'>
+      <div className="overflow-hidden shrink-0 grid grid-cols-1 lg:grid-cols-5  pb-[68px] box-border gap-[0px] lg:gap-[90px]">
+        <div className='col-span-2 px-[16px] lg:px-0'>
           <section className="rounded-2xl bg-apple-style-white-2 overflow-hidden flex flex-col items-center justify-start p-6 gap-[24px] text-left text-9xl text-garbi-version-2-60-black">
-              <div className="flex flex-col items-start justify-start pt-0 px-0 pb-4 gap-[8px] border-b-[1px] border-solid border-garbi-version-2-30-white-70">
+              <div className="flex flex-col w-full items-start justify-start pt-0 px-0 pb-4 gap-[8px] border-b-[1px] border-solid border-garbi-version-2-30-white-70">
                 <div className="relative leading-[120%] font-semibold">
-                  Stake SCR
+                  Stake Ethereum
                 </div>
                 <div className="relative text-xl leading-[120%] text-garbi-version-2-30-white-40 inline-block">
                   Choose how much you want to stake and earn rewards
                 </div>
               </div>
-              <div className="self-stretch rounded-xl bg-apple-style-white-2 mt-[24px] text-xl">
-                <div className='mb-[24px] flex items-center'>
-                  <button className="flex-1 rounded-xl bg-garbi-version-2-30-white shadow-[0px_2px_2px_rgba(0,_0,_0,_0.25)] overflow-hidden flex flex-row items-center justify-start py-2 px-3.5 gap-[12px]">
+              <div className="self-stretch rounded-xl bg-apple-style-white-2 text-xl">
+                <div className='flex items-center'>
+                  <button className="flex-1 rounded-xl bg-garbi-version-2-30-white shadow-[0px_2px_2px_rgba(0,_0,_0,_0.25)] overflow-hidden flex flex-row items-center justify-between px-[14px] py-[8px] gap-[12px]">
                     <div className="relative leading-[120%] text-xl font-semibold">Stake</div>
-                    <div className="flex-1 rounded-lg bg-garbi-version-2-semantic-success overflow-hidden flex flex-row items-center justify-center p-1 text-center text-base text-garbi-version-2-30-white font-inter-sb16">
+                    <div className="rounded-lg bg-garbi-version-2-semantic-success overflow-hidden text-center text-base text-garbi-version-2-30-white font-inter-sb16">
                       <div className="rounded-[8px] leading-[24px] py-[4px] px-[8px] bg-[#49B815] font-semibold">
-                        ~8.5% APY
+                        ~4% APY
                       </div>
                     </div>
                   </button>
@@ -148,10 +148,10 @@ const Stake = () => {
               <div className='self-stretch text-center'>
                 <div className='bg-garbi-version-2-30-white p-[14px] gap-[4px] border-[1px] border-solid border-garbi-version-2-30-white-50 rounded-xl mb-[7px]'>
                   <div className='flex items-center justify-between mb-[6px]'>
-                    <div className=''>
+                    <div className='text-left'>
                       You're staking: $0.00000
                     </div>
-                    <div>
+                    <div className='text-right'>
                       Balance: 0.00000
                     </div>
                   </div>
@@ -216,6 +216,16 @@ const Stake = () => {
                 <div className=''>Unstake in <span className='time-to-unstake'>2</span>Block</div>
                 <div className=''><span className='receive-amount'>0.00</span> SCR</div>
               </div> */}
+              <div className='w-full text-[#666]'>
+                <div className='flex items-center justify-between mb-[4px]'>
+                  <span>1 stETH</span>
+                  <span className='text-black'>= 1.01389654 ETH</span>
+                </div>
+                <div className='flex items-center justify-between'>
+                  <span>Network fee</span>
+                  <span className='text-black'>0.0025 ETH</span>
+                </div>
+              </div>
               <button
                   className="cursor-pointer [border:none] p-4 bg-apple-style-blue-1 self-stretch rounded-lg flex flex-row items-center justify-center hover:bg-lightskyblue-100 active:bg-cornflowerblue"
                   autoFocus={true}
